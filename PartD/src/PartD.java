8d25fb76839ceb1bbefce64864cf9d75dca09469
cdheneree <c.heneree@gmail.com>
2018-09-07 12:19:02 +1200

    Added third line.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Chris.");
 		System.out.println("My favourite colour is green.");
+		System.out.println("My favourite mode of transportion is walking");
 
 	}
 
